27437405a707307f5d95297170fd538e8b10f197
OCSP <58361304+ocsp@users.noreply.github.com>
2020-03-13 10:47:47 +0800

    Create crisp-l.js

---
+++ js/crisp-l.js
@@ -0,0 +1,1 @@
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="a01db678-d71e-4d72-9c07-9693ed973a85";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
